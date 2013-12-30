9b34cf37da329681dbc7fc47081a5cba8f94a72e
J Ross Nicoll <jrn@jrn.me.uk>
2013-12-30 20:45:30 +0000

    Restructured Cryptsy tests to remove non-unit tests.

--- NCryptoExchangeTest/Cryptsy/CryptsyTest.cs
+++ NCryptoExchangeTest/Cryptsy/CryptsyTest.cs
@@ -13,7 +13,7 @@ namespace Lostics.NCryptoExchangeTest.Cryptsy
     public class CryptsyTest
     {
         [TestMethod]
-        public void SignRequestTest()
+        public void TestSignCryptsyRequest()
         {
             string actual;
             string expected = "6dd05bfe3104a70768cf76a30474176db356818d3556e536c31d158fc2c3adafa096df144b46b2ccb1ff6128d6a0a07746695eca061547b25fd676c9614e6718";
@@ -29,98 +29,6 @@ namespace Lostics.NCryptoExchangeTest.Cryptsy
             }
 
             Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void CalculateFeesTest()
-        {
-            Task<Fees> response;
-
-            using (CryptsyExchange cryptsy = GetExchange())
-            {
-                response = cryptsy.CalculateFees(OrderType.Buy, new Price(0.05), new Price(1.0));
-                response.Wait();
-            }
-
-            System.Console.Write(response.Result);
-        }
-
-        [TestMethod]
-        public void GetMarketOrdersTest()
-        {
-            CryptsyMarketId marketId = new CryptsyMarketId("132");
-            Task<List<MarketOrder>> response;
-
-            using (CryptsyExchange cryptsy = GetExchange())
-            {
-                response = cryptsy.GetMarketOrders(marketId);
-                response.Wait();
-            }
-
-            foreach (MarketOrder order in response.Result)
-            {
-                Console.WriteLine(order.ToString());
-            }
-        }
-
-        [TestMethod]
-        public void GetMyOrdersTest()
-        {
-            CryptsyMarketId marketId = new CryptsyMarketId("132");
-            Task<List<MyOrder<CryptsyMarketId, CryptsyOrderId>>> response;
-
-            using (CryptsyExchange cryptsy = GetExchange())
-            {
-                response = cryptsy.GetMyOrders(marketId, null);
-                response.Wait();
-            }
-
-            foreach (MyOrder<CryptsyMarketId, CryptsyOrderId> order in response.Result)
-            {
-                Console.WriteLine(order.ToString());
-            }
-        }
-
-        [TestMethod]
-        public void GetMyTradesTest()
-        {
-            CryptsyMarketId marketId = new CryptsyMarketId("132");
-            Task<List<MyTrade<CryptsyMarketId, CryptsyOrderId, CryptsyTradeId>>> response;
-
-            using (CryptsyExchange cryptsy = GetExchange())
-            {
-                response = cryptsy.GetMyTrades(marketId, null);
-                response.Wait();
-            }
-
-            foreach (MyTrade<CryptsyMarketId, CryptsyOrderId, CryptsyTradeId> trade in response.Result)
-            {
-                Console.WriteLine(trade.ToString());
-            }
-        }
-
-        [TestMethod]
-        public void GetTransactionsTest()
-        {
-            Task<List<Transaction>> response;
-
-            using (CryptsyExchange cryptsy = GetExchange())
-            {
-                response = cryptsy.GetMyTransactions();
-                response.Wait();
-            }
-
-
-            foreach (Transaction transaction in response.Result)
-            {
-                Console.WriteLine(transaction.ToString());
-            }
-        }
-
-        private static CryptsyExchange GetExchange()
-        {
-            return new CryptsyExchange("64d00dc4ee1c2b9551eabbdc831972d4ce2bcac5",
-                "topsecret");
         }
     }
 }
