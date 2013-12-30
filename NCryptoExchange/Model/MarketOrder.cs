048ea9fa4d4f78cf1ec5d808f9dcebc924be3eec
J Ross Nicoll <jrn@jrn.me.uk>
2013-12-30 02:08:21 +0000

    Added common superclass for order classes.

--- NCryptoExchange/Model/MarketOrder.cs
+++ NCryptoExchange/Model/MarketOrder.cs
@@ -2,53 +2,11 @@ using System;
 
 namespace Lostics.NCryptoExchange.Model
 {
-    public class MarketOrder : IComparable<MarketOrder>
+    public class MarketOrder : Order
     {
-        private readonly OrderType orderType;
-        private readonly Quantity price;
-        private readonly Quantity quantity;
-
         public MarketOrder(OrderType orderType, Quantity price, Quantity quantity)
+            : base (orderType, price, quantity)
         {
-            this.orderType = orderType;
-            this.price = price;
-            this.quantity = quantity;
         }
-
-        public int CompareTo(MarketOrder other)
-        {
-            if (this.orderType == other.orderType)
-            {
-                if (this.price.Equals(other.price))
-                {
-                    return this.quantity.CompareTo(other.quantity);
-                }
-                else
-                {
-                    return this.price.CompareTo(other.price);
-                }
-            }
-            else
-            {
-                switch (this.orderType)
-                {
-                    case OrderType.Sell:
-                        return -1;
-                    default:
-                        return 1;
-                }
-            }
-        }
-
-        public override string ToString()
-        {
-            return orderType.ToString().ToUpper() + " "
-                + quantity + " at "
-                + price + " each.";
-        }
-
-        public OrderType OrderType { get { return this.orderType;  } }
-        public Quantity Quantity { get { return this.quantity; } }
-        public Quantity Price { get { return this.price;  } }
     }
 }
---
+++ NCryptoExchange/Model/Order.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lostics.NCryptoExchange.Model
+{
+    public abstract class Order : IComparable<Order>
+    {
+        private readonly OrderType orderType;
+        private readonly Quantity price;
+        private readonly Quantity quantity;
+
+        public  Order(OrderType orderType, Quantity price, Quantity quantity)
+        {
+            this.orderType = orderType;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public int CompareTo(Order other)
+        {
+            if (this.orderType == other.orderType)
+            {
+                if (this.price.Equals(other.price))
+                {
+                    return this.quantity.CompareTo(other.quantity);
+                }
+                else
+                {
+                    return this.price.CompareTo(other.price);
+                }
+            }
+            else
+            {
+                switch (this.orderType)
+                {
+                    case OrderType.Sell:
+                        return -1;
+                    default:
+                        return 1;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return orderType.ToString().ToUpper() + " "
+                + quantity + " at "
+                + price + " each.";
+        }
+
+        public OrderType OrderType { get { return this.orderType;  } }
+        public Quantity Quantity { get { return this.quantity; } }
+        public Quantity Price { get { return this.price;  } }
+    }
+}
