3fca766c39624c55089a4df5ac1e7ccb424d36a4
J Ross Nicoll <jrn@jrn.me.uk>
2014-01-08 11:38:56 +0000

    Trade recording to CSV now has basic functionality completed.

--- ExampleTransactionsToCSV/ExampleTradesToCSV.cs
+++ ExampleTransactionsToCSV/ExampleTradesToCSV.cs
@@ -21,16 +21,39 @@ namespace Lostics.NCryptoExample
         static void Main()
         {
             CryptsyExchange cryptsy = CryptsyExchange.GetExchange(GetDefaultConfigurationFile());
+            string filename = DateTime.Now.ToFileTime() + ".csv";
+            FileInfo file = new FileInfo(
+                Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName,
+                filename)
+            );
 
-            foreach (MyTrade<CryptsyMarketId, CryptsyOrderId> trade in cryptsy.GetAllMyTrades(null).Result)
+            using (FileStream stream = new FileStream(file.FullName, FileMode.CreateNew))
             {
-                Console.WriteLine(trade.TradeId.ToString() + ","
-                    + Enum.GetName(typeof(OrderType), trade.TradeType)
-                    );
+                using (TextWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("Trade ID, Date, Order, Market, Price, Quantity, Fee");
+                    WriteTradesToCSV(writer, cryptsy, cryptsy.GetAllMyTrades(null).Result);
+                }
             }
 
             Console.WriteLine("Requests completed, press any key to exit.");
             Console.ReadKey();
+        }
+
+        private static void WriteTradesToCSV(TextWriter writer, CryptsyExchange exchange,
+            List<MyTrade<CryptsyMarketId, CryptsyOrderId>> trades)
+        {
+            foreach (MyTrade<CryptsyMarketId, CryptsyOrderId> trade in trades)
+            {
+                writer.WriteLine(trade.TradeId.ToString() + ","
+                    + trade.DateTime + ","
+                    + Enum.GetName(typeof(OrderType), trade.TradeType) + ","
+                    + trade.MarketId + ","
+                    + trade.Price + ","
+                    + trade.Quantity + ","
+                    + trade.Fee
+                );
+            }
         }
     }
 }
